ce8defdeddfbffa47d1cbab037e00ae859e37682
NietoCurcio <felipe_nieto010@hotmail.com>
2020-12-26 21:22:40 -0300

    random quote - FCC

--- script.js
+++ script.js
@@ -9,11 +9,6 @@ const App = () => {
   const url =
     'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
 
-  const getData = async () => {
-    const data = await axios.get(url)
-    setQuotes(data.data.quotes)
-  }
-
   const setRandomMessage = () => {
     const quote = quotes[Math.floor(Math.random() * quotes.length)]
     setQuoteSelected({ message: quote.quote, author: quote.author })
